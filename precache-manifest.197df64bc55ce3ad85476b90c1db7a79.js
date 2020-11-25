4994c0151bc6565b594518d09d4037d29dfd6e7b
NikhilCodes <nikhil.nixel@gmail.com>
2020-11-25 23:31:10 +0530

    Updates

---
+++ precache-manifest.197df64bc55ce3ad85476b90c1db7a79.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd13b8d92d0bd26206e9d9881a438e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c858c6bd3b93f9f02f37",
+    "url": "/static/css/main.54cc5c25.chunk.css"
+  },
+  {
+    "revision": "6a4e64af1304c7fb5e0a",
+    "url": "/static/js/2.f4172188.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.f4172188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54581bcd16c3d50f9cc",
+    "url": "/static/js/3.d895d1b2.chunk.js"
+  },
+  {
+    "revision": "c858c6bd3b93f9f02f37",
+    "url": "/static/js/main.c455f232.chunk.js"
+  },
+  {
+    "revision": "68758321800b3b425132",
+    "url": "/static/js/runtime-main.05a17934.js"
+  },
+  {
+    "revision": "662b97d6b932baf803e01d567ab72e70",
+    "url": "/static/media/Street Cred.662b97d6.ttf"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/static/media/email.2c1a7560.png"
+  },
+  {
+    "revision": "d5c378c566c11d31b4ecd90595e7c8c4",
+    "url": "/static/media/github.d5c378c5.png"
+  },
+  {
+    "revision": "9aeaef887e61df7ca8b3c6691d768a09",
+    "url": "/static/media/linkedin.9aeaef88.png"
+  },
+  {
+    "revision": "c8e83b869a1b39cc2f2c6ea2e3032280",
+    "url": "/static/media/resume.c8e83b86.pdf"
+  }
+]);
